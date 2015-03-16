f5a57ddbd2ca7e4d04354b3d48c9e8883ec0acfe
Rizmari Versfeld <rizziepit@gmail.com>
2015-03-16 15:38:12 +0200

    add same_origin function

--- unicore/hub/client/utils.py
+++ unicore/hub/client/utils.py
@@ -1,3 +1,10 @@
+from urlparse import urlparse
+
+
+PROTOCOL_TO_PORT = {
+    'http': 80,
+    'https': 443,
+}
 
 
 def client_from_config(
@@ -7,3 +14,17 @@ def client_from_config(
                     if key.startswith(prefix))
     settings.update(kwargs)
     return client_cls(**settings)
+
+
+def same_origin(url1, url2):
+    ''' Return True if the urls have the same origin, else False.
+    Copied from Django:
+    https://github.com/django/django/blob/master/django/utils/http.py#L255
+    '''
+    p1, p2 = urlparse(url1), urlparse(url2)
+    try:
+        o1 = (p1.scheme, p1.hostname, p1.port or PROTOCOL_TO_PORT[p1.scheme])
+        o2 = (p2.scheme, p2.hostname, p2.port or PROTOCOL_TO_PORT[p2.scheme])
+        return o1 == o2
+    except (ValueError, KeyError):
+        return False
