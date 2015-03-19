96e53648aef794f9dd3cb14cc0db6c037cfe36fe
Rizmari Versfeld <rizziepit@gmail.com>
2015-03-19 11:52:16 +0200

    declare __all__ so that pep8 is fine

--- unicore/hub/client/__init__.py
+++ unicore/hub/client/__init__.py
@@ -1,2 +1,8 @@
 from unicore.hub.client.base import ClientException
 from unicore.hub.client.userclient import UserClient, User
+
+
+__all__ = [
+    'ClientException',
+    'User',
+    'UserClient']
